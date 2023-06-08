295ba4ce1b3516f844ebfb0cfaa0a8c5f1678d89
MrGoopyDrawers <107856947+MrGoopyDrawers@users.noreply.github.com>
2023-06-08 09:02:23 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace mrbeast;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "36.1";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "Mrbeast";
 
     public const string Description = "Adds MrBeast tower to the game.";
